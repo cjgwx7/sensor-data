d96f64ef21c034fc13a1c146a65cdd8408dd246f
cjgwx7 <cgrohmann@mail.missouri.edu>
2022-05-03 17:35:30 -0500

    treatment proportion single var

--- scripts/r/single-variable/TreatmentProportion-SingleVariable.R
+++ scripts/r/single-variable/TreatmentProportion-SingleVariable.R
@@ -1,39 +1,63 @@
 library(dplyr)
 library(lmerTest)
+library(tidyr)
+
+rm(list = ls())
 
 load("/storage/hpc/group/deckerlab/cjgwx7/sensor-data/data/master-file-clean-lagged.RData") # nolint
 
 df2 <- df %>%
-     dplyr::select(-c(WaterMedicationType, Comments, Days, Inventory, TreatmentProportion)) # nolint
+    dplyr::select(-c(GlobalID:Turn, CalendarDate:NumericDate,
+                     WaterMedicationType, Comments, TreatmentProportion)) # nolint
 
 df3 <- df %>%
-    dplyr::select(Days, Inventory, TreatmentProportion)
+    dplyr::select(TreatmentProportion)
 
 fit_list <- list()
 
-for (i in c(11:ncol(df2))) {
+for (i in c(5:ncol(df2))) {
 
     fit_df <- df2 %>%
-        dplyr::select(Order, Site_Room_Turn, i) %>%
-        cbind(., df3)
-    ind_var <- colnames(fit_df)[3]
-    fit_formula <- formula(paste("TreatmentProportion ~ ", ind_var, " + (1 + Days + I(Days^2)| Site_Room_Turn)", sep = "")) # nolint
-    print(paste(ind_var, "Complete"))
-    fit_summary <- coef(summary(glmer(fit_formula, family = binomial(), weights = Inventory, data = fit_df))) # nolint
+        dplyr::select(Order, Site_Room_Turn, Days, Inventory, i) %>%
+        cbind(., df3) %>%
+        tidyr::drop_na(.)
+
+    null_fit <- glmer(TreatmentProportion ~ (1 + Days + I(Days^2) + I(Days^3) | Site_Room_Turn), # nolint
+                      family = binomial,
+                      weights = Inventory,
+                      data = fit_df)
+
+    ind_var <- colnames(fit_df)[5]
+    fit_formula <- formula(paste("TreatmentProportion ~ ", ind_var, " + (1 + Days + I(Days^2) + I(Days^3) | Site_Room_Turn)", sep = "")) # nolint
+    fit <- glmer(fit_formula,
+                 family = binomial,
+                 weights = Inventory,
+                 data = fit_df)
+    fit_summary <- coef(summary(fit))
+    fit_lrt <- anova(null_fit, fit)
     summary_df <- data.frame(DepVar = "TreatmentProportion",
                              IndVar = ind_var,
                              Estimate = fit_summary[2, 1],
                              SE = fit_summary[2, 2],
-                             z = fit_summary[2, 3],
-                             P = fit_summary[2, 4])
+                             WaldZ_Statistic = fit_summary[2, 3],
+                             WaldTest_P = fit_summary[2, 4],
+                             N_Parameters = fit_lrt[2, 1],
+                             NullAIC = fit_lrt[1, 2],
+                             TestAIC = fit_lrt[2, 2],
+                             NullLikelihood = fit_lrt[1, 4],
+                             FitLikelihood = fit_lrt[2, 4],
+                             LRT_Statistic = fit_lrt[2, 6],
+                             LRTdf = fit_lrt[2, 7],
+                             LRT_P = fit_lrt[2, 8])
     fit_list[[i]] <- summary_df
+    print(paste("The model for variable", ind_var, "was successfully fit.", sep = " ")) # nolint
 
 }
 
 df_out <- bind_rows(fit_list)
 
 write.csv(df_out,
-          file = "/group/deckerlab/cjgwx7/sensor-data/results/TreatmentProportion-SingleVariable.csv", # nolint
+          file = "/group/deckerlab/cjgwx7/sensor-data/results/single-variable/TreatmentProportion-SingleVariable.csv", # nolint
           sep = ",",
           row.names = FALSE,
           col.names = TRUE)
